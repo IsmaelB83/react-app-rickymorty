c886a240cce33b251723ff64a05ee28fcc4a8629
Ismael Bernal <ismaelbernal83@gmail.com>
2019-04-14 19:42:43 +0200

    Adding fontawesome react library

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,10 @@
 // Node imports
 import React, { Component } from 'react';
 import ta from 'time-ago';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faSkull, faQuestion } from '@fortawesome/free-solid-svg-icons'
+import { faMale, faFemale, faTransgender } from '@fortawesome/free-solid-svg-icons'
+import { faRadiation, faMagic } from '@fortawesome/free-solid-svg-icons'
 // CSS imports
 import './Card.css';
 
@@ -19,9 +23,6 @@ export default class CharacterCard extends Component {
     };
   }
   
-  componentWillMount() {
-  }
-  
   render() {
     return (
       <div className="card">
@@ -33,9 +34,9 @@ export default class CharacterCard extends Component {
           </div>
         </div>
         <div className="card-body">
-          <p><span>Status</span><span>{this.state.status}</span></p>
-          <p><span>Species</span><span>{this.state.species}</span></p>
-          <p><span>Gender</span><span>{this.state.gender}</span></p>
+          <p><span>Status</span><span>{this.state.status} <FontAwesomeIcon className="fontIcon" icon={this.state.status==='Dead'?faSkull:this.state.status==='Alive'?faHeart:faQuestion} /></span></p>
+          <p><span>Species</span><span>{this.state.species} <FontAwesomeIcon className="fontIcon" icon={this.state.species==='Human' || this.state.species==='Humanoid'?faMale:this.state.species==='Alien'?faRadiation:this.state.species==='Mytholog'?faMagic:faQuestion} /></span></p>
+          <p><span>Gender</span><span>{this.state.gender} <FontAwesomeIcon className="fontIcon" icon={this.state.gender==='Male'?faMale:this.state.gender==='Female'?faFemale:this.state.gender==='Genderless'?faTransgender:faQuestion} /></span></p>
           <p><span>Origin</span><span>{this.state.origin}</span></p>
           <p><span>Last Location</span><span>{this.state.last}</span></p>
         </div>
